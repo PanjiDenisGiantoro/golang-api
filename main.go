93d2772972858e7c00d06eaa4a35e4bccdfaeba5
PanjiDenisGiantoro <panjidenisgiantoroo@gmail.com>
2023-12-09 11:59:05 +0700

    push

--- main.go
+++ main.go
@@ -2,10 +2,83 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"log"
+	"webapi2/book"
 	"webapi2/handler"
 )
 
 func main() {
+	dsn := "root:W@rung01@tcp(127.0.0.1:3306)/golang?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	db.AutoMigrate(&book.Book{})
+
+	//create
+
+	//book := book.Book{}
+	//book.Title = "Golang 2"
+	//book.Price = 20000
+	//book.Discount = 30
+	//book.Rating = 9
+	//book.Description = "Golang is the best wkwkw"
+	//
+	//err = db.Create(&book).Error
+	//if err != nil {
+	//	fmt.Println(err)
+	//}
+
+	//read
+	//book := book.Book{}
+
+	//find by id
+	//var book book.Book
+	//err = db.Debug().First(&book).Error
+
+	//get
+	//var books []book.Book
+	//err = db.Debug().Find(&books).Error
+	//if err != nil {
+	//	fmt.Println(err)
+	//}
+
+	//for _, b := range books {
+	//	fmt.Println("title", b.Title)
+	//	fmt.Println("price %v", b)
+	//
+	//}
+
+	//update
+	//var book book.Book
+	//
+	//err = db.Debug().Where("id = ?", 1).First(&book).Error
+	//if err != nil {
+	//	fmt.Println(err)
+	//}
+	//book.Title = "Golang 2"
+	//book.Price = 20000
+	//book.Discount = 30
+	//book.Rating = 9
+	//book.Description = "Golang is the best wkwkw"
+	//err = db.Save(&book).Error
+	//if err != nil {
+	//	fmt.Println(err)
+	//}
+
+	//delete
+	var book book.Book
+
+	err = db.Debug().Where("id = ?", 1).Delete(&book).Error
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//fmt.Println("title", book.Title)
+	//fmt.Println("price %v", book)
+
 	router := gin.Default()
 	v1 := router.Group("/v1")
 	v1.GET("/ping", handler.HandleRoot)
